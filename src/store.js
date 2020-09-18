24a7b0a921a99db1a78756c07da79ba70cf9b51b
Adrian Marquis <adrian.marquis78@gmail.com>
2020-09-18 12:53:55 -0400

    first commit

---
+++ src/store.js
@@ -0,0 +1,21 @@
+const { v4: uuid } = require('uuid');
+
+const bookmarks = [
+  { id: uuid(),
+    title: 'GitHub',
+    url: 'https://www.github.com',
+    description: 'Store and implore',
+    rating: 5 },
+  { id: uuid(),
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: uuid(),
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
+]
+
+module.exports = { bookmarks }
