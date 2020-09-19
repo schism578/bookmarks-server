ce14296c70f31e9ad0272338dff05392f5216e29
Adrian Marquis <adrian.marquis78@gmail.com>
2020-09-19 09:23:29 -0400

    refactored for DELETE and POST

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
+const bookmarksRouter = require('./bookmark/bookmark-router')
 
 const app = express()
 
